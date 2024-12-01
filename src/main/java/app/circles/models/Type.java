43f7a27844d17abd8b27c6bfa5363f8a5f69ef4a
lenaevd <lenaevd0kim0va@yandex.ru>
2024-12-02 01:59:26 +0300

    changed table name

--- src/main/java/app/circles/models/Type.java
+++ src/main/java/app/circles/models/Type.java
@@ -2,14 +2,17 @@ package app.circles.models;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
-@Table(name = "type")
+@Table(name = "types")
 public class Type {
     @Id
+    @GeneratedValue
     private Integer id;
     private String name;
 }
